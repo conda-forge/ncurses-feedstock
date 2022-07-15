566a289c7040e6d609d30620954eb5702df4760b
Jonas Haag <jonas@lophus.org>
2022-07-15 17:08:34 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 
 :: From https://github.com/msys2/MSYS2-packages/blob/master/ncurses/PKGBUILD
 bash -c "echo before sed; ls -lR"
-bash -c 'sed -i \'s!eval `${MAKE-make} -f conftest.make 2>/dev/null | grep temp=`!${MAKE-make} -f conftest.make 2>/dev/null!g\' configure'
+bash -c $'sed -i \'s!eval `${MAKE-make} -f conftest.make 2>/dev/null | grep temp=`!${MAKE-make} -f conftest.make 2>/dev/null!g\' configure'
 bash -c "echo after sed"
 if errorlevel 1 exit 1
 
