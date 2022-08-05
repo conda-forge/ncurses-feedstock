82620764eba5225e94e02705edc90cff23acb8ae
Matthias Diener <mdiener@illinois.edu>
2022-08-05 13:10:01 -0500

    t

--- recipe/bld.sh
+++ recipe/bld.sh
@@ -4,10 +4,10 @@ set -ex
 
 # set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\share\pkgconfig
 
-sed -i 's!eval `${MAKE-make} -f conftest.make 2>/dev/null | grep temp=`!${MAKE-make} -f conftest.make 2>/dev/null!g' configure
+# sed -i 's!eval `${MAKE-make} -f conftest.make 2>/dev/null | grep temp=`!${MAKE-make} -f conftest.make 2>/dev/null!g' configure
 
 # Some options copied from https://github.com/msys2/MSYS2-packages/blob/master/ncurses/PKGBUILD
-bash configure \
+./configure \
   --disable-mixed-case \
   --disable-overwrite \
   --disable-relink \
@@ -36,6 +36,8 @@ bash configure \
   --without-progs \
   --without-termlib \
   --without-tests
+
+cat include/Makefile
 
 make
 make install
