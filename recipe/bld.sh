86b1551c01552f651b68df0e22bd61215a795325
Matthias Diener <mdiener@illinois.edu>
2022-08-05 13:50:13 -0500

    t

--- recipe/bld.sh
+++ recipe/bld.sh
@@ -4,7 +4,7 @@ set -ex
 
 # set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\share\pkgconfig
 
-# sed -i 's!eval `${MAKE-make} -f conftest.make 2>/dev/null | grep temp=`!${MAKE-make} -f conftest.make 2>/dev/null!g' configure
+sed -i 's!eval `${MAKE-make} -f conftest.make 2>/dev/null | grep temp=`!${MAKE-make} -f conftest.make 2>/dev/null!g' configure
 
 # Some options copied from https://github.com/msys2/MSYS2-packages/blob/master/ncurses/PKGBUILD
 ./configure \
