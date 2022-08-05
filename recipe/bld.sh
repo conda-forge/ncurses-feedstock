c516ab214405df930acba98d14b25d2025d3eff8
Matthias Diener <mdiener@illinois.edu>
2022-08-05 12:51:00 -0500

    t

--- recipe/bld.sh
+++ recipe/bld.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
+set -ex
+
 # set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\share\pkgconfig
 
 sed -i 's!eval `${MAKE-make} -f conftest.make 2>/dev/null | grep temp=`!${MAKE-make} -f conftest.make 2>/dev/null!g' configure
 
 # Some options copied from https://github.com/msys2/MSYS2-packages/blob/master/ncurses/PKGBUILD
-bash -x configure \
+bash configure \
   --disable-mixed-case \
   --disable-overwrite \
   --disable-relink \
@@ -16,7 +20,7 @@ bash -x configure \
   --enable-ext-mouse \
   --enable-pc-files \
   --enable-sigwinch \
-  --enable-sp-funcs 
+  --enable-sp-funcs \
   --enable-tcap-names \
   --enable-widec \
   --with-cxx-shared \
@@ -35,7 +39,7 @@ bash -x configure \
 
 make
 make install
-  
+
 # --with-default-terminfo-dir=/usr/share/terminfo
 #   --includedir=/usr/include/ncursesw
 # --with-build-cflags=-D_XOPEN_SOURCE_EXTENDED \
