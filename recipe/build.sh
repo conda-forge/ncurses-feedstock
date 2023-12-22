4f8eed082f1c0a8a8665b51011b727751c2f2f27
Marcel Bargull <marcel.bargull@udo.edu>
2023-12-22 18:50:11 +0100

    Build only wide character library variants
    But still provide symlinks for <lib>w for backward compatibility.
    
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,71 +13,63 @@ if [[ $target_platform =~ osx-.* ]]; then
     export cf_cv_mixedcase=no
 fi
 
-for USE_WIDEC in false true;
-do
-    WIDEC_OPT=""
-    w=""
-    if [ "${USE_WIDEC}" = true ];
-    then
-        WIDEC_OPT="--enable-widec"
-        w="w"
-    fi
+export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig
 
-    export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig
+sh ./configure \
+    --prefix=$PREFIX \
+    --without-debug \
+    --without-ada \
+    --without-manpages \
+    --with-shared \
+    --with-pkg-config \
+    --with-pkg-config-libdir="${PKG_CONFIG_LIBDIR}" \
+    --disable-overwrite \
+    --enable-symlinks \
+    --enable-termcap \
+    --enable-pc-files \
+    --with-termlib \
+    --with-versioned-syms \
+    --enable-widec \
+    --disable-lib-suffixes
 
-    sh ./configure \
-	    --prefix=$PREFIX \
-	    --without-debug \
-	    --without-ada \
-	    --without-manpages \
-	    --with-shared \
-	    --with-pkg-config \
-	    --with-pkg-config-libdir=$PREFIX/lib/pkgconfig \
-	    --disable-overwrite \
-	    --enable-symlinks \
-	    --enable-termcap \
-	    --enable-pc-files \
-	    --with-termlib \
-	    --with-versioned-syms \
-	    $WIDEC_OPT
+if [[ "$target_platform" == osx* ]]; then
+    # When linking libncurses*.dylib, reexport libtinfo so that later
+    # client code linking against just -lncurses also gets -ltinfo.
+    sed -i.orig '/^SHLIB_LIST/s/-ltinfo/-Wl,-reexport&/' ncurses/Makefile
+fi
 
-    if [[ "$target_platform" == osx* ]]; then
-        # When linking libncurses*.dylib, reexport libtinfo[w] so that later
-        # client code linking against just -lncurses[w] also gets -ltinfo[w].
-        sed -i.orig '/^SHLIB_LIST/s/-ltinfo/-Wl,-reexport&/' ncurses/Makefile
-    fi
+make -j ${CPU_COUNT}
+make INSTALL="${BUILD_PREFIX}/bin/install -c  --strip-program=${STRIP}" install
+make clean
+make distclean
 
-    make -j ${CPU_COUNT}
-    make INSTALL="${BUILD_PREFIX}/bin/install -c  --strip-program=${STRIP}" install
-    make clean
-    make distclean
+# Provide headers in $PREFIX/include/ncurses and symlink them in
+# $PREFIX/include and $PREFIX/include/ncursesw.
+ln -s -L -r "${PREFIX}/include/ncurses/"* -t "${PREFIX}/include/"
+ln -s -L -r "${PREFIX}/include/ncurses" "${PREFIX}/include/ncursesw"
 
-    # Provide headers in `$PREFIX/include` and
-    # symlink them in `$PREFIX/include/ncurses`
-    # and in `$PREFIX/include/ncursesw`.
-    HEADERS_DIR="${PREFIX}/include/ncurses"
-    if [ "${USE_WIDEC}" = true ];
-    then
-        HEADERS_DIR="${PREFIX}/include/ncursesw"
-    fi
-    for HEADER in $(ls $HEADERS_DIR);
-    do
-        mv "${HEADERS_DIR}/${HEADER}" "${PREFIX}/include/${HEADER}"
-        ln -s "${PREFIX}/include/${HEADER}" "${HEADERS_DIR}/${HEADER}"
+if [[ "$target_platform" != osx* ]]; then
+    # Replace the installed libncurses.so with a linker script
+    # so that linking against it also brings in -ltinfo.
+    DEVLIB=$PREFIX/lib/libncurses.so
+    RUNLIB=$(basename $(readlink $DEVLIB))
+    rm $DEVLIB
+    echo "INPUT($RUNLIB -ltinfo)" > $DEVLIB
+fi
+
+shared_libs='ncurses tinfo form menu panel'
+for lib in ${shared_libs}; do
+    lib_name="lib${lib}"
+    for lib_file in "${PREFIX}/lib/${lib_name}"*"${SHLIB_EXT}"*; do
+        # Provide symlinks for explicitly named wide char varities for
+        # backwards compatibility.
+        ln -s -L -r "${lib_file}" "${lib_file%%${lib_name}*}${lib_name}w${lib_file##*${lib_name}}"
     done
-
-    if [[ "$target_platform" != osx* ]]; then
-        # Replace the installed libncurses[w].so with a linker script
-        # so that linking against it also brings in -ltinfo[w].
-        DEVLIB=$PREFIX/lib/libncurses$w.so
-        RUNLIB=$(basename $(readlink $DEVLIB))
-        rm $DEVLIB
-        echo "INPUT($RUNLIB -ltinfo$w)" > $DEVLIB
-    fi
+    # Explicitly delete static libraries
+    rm "${PREFIX}/lib/${lib_name}.a"
 done
 
-# Explicitly delete static libraries
-for LIB_NAME in libncurses libtinfo libform libmenu libpanel; do
-    rm ${PREFIX}/lib/${LIB_NAME}.a
-    rm ${PREFIX}/lib/${LIB_NAME}w.a
+for lib in ${shared_libs} ncurses++; do
+    ln -s -L -r "${PKG_CONFIG_LIBDIR}/${lib}"{,w}".pc"
 done
+ln -s -L -r "${PREFIX}/bin/ncurses"{,w}"${PKG_VERSION%%.*}-config"
