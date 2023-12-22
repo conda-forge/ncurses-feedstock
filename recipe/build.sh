9550eb02f38548a5bf419ef461243c4715214337
Marcel Bargull <marcel.bargull@udo.edu>
2023-12-22 21:11:12 +0100

    Try wide-char-named libraries as base
    Linux builds failed while linking the internal tests with
      ncurses.c:(.text.show_wbox_chars+0x92): undefined reference to `_nc_wacs'
    No idea why that symbol is missing...
    
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- recipe/build.sh
+++ recipe/build.sh
@@ -29,13 +29,12 @@ sh ./configure \
     --enable-pc-files \
     --with-termlib \
     --with-versioned-syms \
-    --enable-widec \
-    --disable-lib-suffixes
+    --enable-widec
 
 if [[ "$target_platform" == osx* ]]; then
-    # When linking libncurses*.dylib, reexport libtinfo so that later
-    # client code linking against just -lncurses also gets -ltinfo.
-    sed -i.orig '/^SHLIB_LIST/s/-ltinfo/-Wl,-reexport&/' ncurses/Makefile
+    # When linking libncurses*.dylib, reexport libtinfow so that later
+    # client code linking against just -lncursesw also gets -ltinfow.
+    sed -i.orig '/^SHLIB_LIST/s/-ltinfow/-Wl,-reexport&/' ncurses/Makefile
 fi
 
 make -j ${CPU_COUNT}
@@ -43,33 +42,33 @@ make INSTALL="${BUILD_PREFIX}/bin/install -c  --strip-program=${STRIP}" install
 make clean
 make distclean
 
-# Provide headers in $PREFIX/include/ncurses and symlink them in
-# $PREFIX/include and $PREFIX/include/ncursesw.
-ln -s -L -r "${PREFIX}/include/ncurses/"* -t "${PREFIX}/include/"
-ln -s -L -r "${PREFIX}/include/ncurses" "${PREFIX}/include/ncursesw"
+# Provide headers in $PREFIX/include/ncursesw and symlink them in
+# $PREFIX/include and $PREFIX/include/ncurses.
+ln -s -L -r "${PREFIX}/include/ncursesw/"* -t "${PREFIX}/include/"
+ln -s -L -r "${PREFIX}/include/ncursesw" "${PREFIX}/include/ncurses"
 
 if [[ "$target_platform" != osx* ]]; then
-    # Replace the installed libncurses.so with a linker script
-    # so that linking against it also brings in -ltinfo.
-    DEVLIB=$PREFIX/lib/libncurses.so
+    # Replace the installed libncursesw.so with a linker script
+    # so that linking against it also brings in -ltinfow.
+    DEVLIB=$PREFIX/lib/libncursesw.so
     RUNLIB=$(basename $(readlink $DEVLIB))
     rm $DEVLIB
-    echo "INPUT($RUNLIB -ltinfo)" > $DEVLIB
+    echo "INPUT($RUNLIB -ltinfow)" > $DEVLIB
 fi
 
 shared_libs='ncurses tinfo form menu panel'
 for lib in ${shared_libs}; do
     lib_name="lib${lib}"
-    for lib_file in "${PREFIX}/lib/${lib_name}"*"${SHLIB_EXT}"*; do
-        # Provide symlinks for explicitly named wide char varities for
+    for lib_file in "${PREFIX}/lib/${lib_name}w"*"${SHLIB_EXT}"*; do
+        # Provide symlinks for non-wide char varities for
         # backwards compatibility.
-        ln -s -L -r "${lib_file}" "${lib_file%%${lib_name}*}${lib_name}w${lib_file##*${lib_name}}"
+        ln -s -L -r "${lib_file}" "${lib_file%%${lib_name}w*}${lib_name}${lib_file##*${lib_name}w}"
     done
     # Explicitly delete static libraries
-    rm "${PREFIX}/lib/${lib_name}.a"
+    rm "${PREFIX}/lib/${lib_name}w.a"
 done
 
 for lib in ${shared_libs} ncurses++; do
-    ln -s -L -r "${PKG_CONFIG_LIBDIR}/${lib}"{,w}".pc"
+    ln -s -L -r "${PKG_CONFIG_LIBDIR}/${lib}"{w,}".pc"
 done
-ln -s -L -r "${PREFIX}/bin/ncurses"{,w}"${PKG_VERSION%%.*}-config"
+ln -s -L -r "${PREFIX}/bin/ncurses"{w,}"${PKG_VERSION%%.*}-config"
